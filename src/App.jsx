a792a651d0799a01870039d23b2161f4a2b26ecd
dev-harleen <harleenk.dev@gmail.com>
2024-08-21 12:46:15 -0700

    add header code

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { Image } from "@mui/icons-material";
+import Intro from "./components/Intro";
+import { ImageListItem } from "@mui/material";
+import Header from "./components/header/Header";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Intro />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Intro.jsx
@@ -0,0 +1,22 @@
+import { Box } from "@mui/material";
+
+export default function Intro() {
+  return (
+    <Box sx={{ p: 10 }}>
+      <h1>Hi, Harleen! </h1>
+      <p>I'm a software developer</p>
+      <p>
+        As a software developer, you specialize in designing, coding, and <br />
+        maintaining software applications. You have experience in crafting
+        <br />
+        solutions that address user needs and solve complex problems. Proficient
+        <br />
+        in various programming languages and tools, you excel at turning <br />
+        concepts into functional, scalable software. Your work involves <br />
+        collaborating with teams to ensure software meets both user and business
+        <br />
+        requirements while continuously improving and optimizing code.
+      </p>
+    </Box>
+  );
+}
