f0bc0a2d57e7e1850b64819a28f0988d39369925
dev-harleen <harleenk.dev@gmail.com>
2025-02-27 22:08:43 -0800

    changes for about section and added missing svgs

--- src/components/header/Navbar.tsx
+++ src/components/header/Navbar.tsx
@@ -1,41 +1,41 @@
-import React from "react";
-import { NavLink } from "react-router-dom";
+// import React from "react";
+// import {Nav, Navbar} from "react-bootstrap";
 
-export const Navbar = () => {
-  return (
-    <>
-      <nav className="navbar navbar-expand-lg navbar-light bg-light sticky-top">
-        <a className="navbar-brand ms-5" href="#">
-          HK
-        </a>
-        <button
-          className="navbar-toggler order-2"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarNavAltMarkup"
-          aria-controls="navbarNavAltMarkup"
-          aria-expanded="false"
-          aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-          <div className="navbar-nav ms-auto me-lg-5">
-            <NavLink className="nav-item nav-link active me-5" to="/">
-              Home
-            </NavLink>
-            <NavLink className="nav-item nav-link me-5" to="/about">
-              About
-            </NavLink>
-            <NavLink className="nav-item nav-link me-5" to="/work">
-              Work
-            </NavLink>
-            <NavLink className="nav-item nav-link" to="/contact">
-              Contact
-            </NavLink>
-          </div>
-        </div>
-        <button className="me-md-3 order-1">Theme</button>
-      </nav>
-    </>
-  );
-};
+// export const Navbar = () => {
+//   return (
+  
+//       <Navbar className="navbar navbar-expand-lg navbar-light bg-light sticky-top">
+//         <a className="navbar-brand ms-5" href="#">
+//           HK
+//         </a>
+//         <button
+//           className="navbar-toggler order-2"
+//           type="button"
+//           data-toggle="collapse"
+//           data-target="#navbarNavAltMarkup"
+//           aria-controls="navbarNavAltMarkup"
+//           aria-expanded="false"
+//           aria-label="Toggle navigation">
+//           <span className="navbar-toggler-icon"></span>
+//         </button>
+//         <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+//           <Nav className="navbar-nav ms-auto me-lg-5">
+//             <Nav.Link className="nav-item nav-link active me-5" href="/">
+//               Home
+//             </Nav.Link>
+//             <Nav.Link className="nav-item nav-link me-5" href="/about">
+//               About
+//             </Nav.Link>
+//             <Nav.Link className="nav-item nav-link me-5" href="/work">
+//               Work
+//             </Nav.Link>
+//             <Nav.Link className="nav-item nav-link" href="/contact">
+//               Contact
+//             </Nav.Link>
+//           </Nav>
+//         </div>
+//         <button className="me-md-3 order-1">Theme</button>
+//       </Navbar>
+    
+//   );
+// };
